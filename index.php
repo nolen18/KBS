412cdf22c8a6694af40b4f8104b90e8e0c591efe
nolen18 <nolenlamboo16@gmail.com>
2017-11-15 21:33:15 +0100

    1
    1

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WOW</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<script type="text/javascript" src="assets/js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+	
+</head>
+<body id="bootstrap">
+	<?php 
+	include 'assets/view/nav.html';
+	 ?>
+
+
+
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="http://via.placeholder.com/1920x1080" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="http://via.placeholder.com/1920x1080" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="http://via.placeholder.com/1920x1080" alt="Third slide">
+    </div>
+  </div>
+</div>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-3">
+      <img src="http://via.placeholder.com/250x300">
+    </div>
+    <div class="col-md-3">
+      <img src="http://via.placeholder.com/250x300">
+    </div>
+    <div class="col-md-3">
+     <img src="http://via.placeholder.com/250x300">
+    </div>
+    <div class="col-md-3">
+      <img src="http://via.placeholder.com/250x300">
+    </div>
+  </div>
+</div>	
+	
+</div>
+
+
+</body>
+</html>
